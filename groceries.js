b43871961cb538ab0ba0f061786928dbedf3246d
Hayley Goodman <31858311+hayleyg5@users.noreply.github.com>
2020-06-02 23:21:52 -0400

    Update groceries.js

--- groceries.js
+++ groceries.js
@@ -104,11 +104,10 @@ var products = [
 
 function restrictListProducts(prods, restriction) {
 	let product_names = [];
-	let product_prices = [];
 	for (let i=0; i<prods.length; i+=1) {
 		if ((restriction == "Vegetarian") && (prods[i].vegetarian == true)){
 			product_names.push(prods[i].name);
-			product_prices.push(prods[i].price);
+			product_names.append(prods[i].price);
 		}
 		else if ((restriction == "GlutenFree") && (prods[i].glutenFree == true)){
 			product_names.push(prods[i].name);
@@ -122,7 +121,7 @@ function restrictListProducts(prods, restriction) {
 		}
 	}
 		
-	return [product_names, product_prices];
+	return product_names;
 }
 
 // Calculate the total price of items, with received parameter being a list of products
