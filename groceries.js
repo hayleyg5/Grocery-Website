a3e97c033b83f58f5a643ca9441a1f7956713e44
Hayley Goodman <31858311+hayleyg5@users.noreply.github.com>
2020-06-02 21:26:16 -0400

    Update groceries.js

--- groceries.js
+++ groceries.js
@@ -106,7 +106,7 @@ function restrictListProducts(prods, restriction) {
 	let product_names = [];
 	for (let i=0; i<prods.length; i+=1) {
 		if ((restriction == "Vegetarian") && (prods[i].vegetarian == true)){
-			product_names.push(prods[i].name);
+			product_names.push(prods[i].name, ":", prods[i].price);
 		}
 		else if ((restriction == "GlutenFree") && (prods[i].glutenFree == true)){
 			product_names.push(prods[i].name);
