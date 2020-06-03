6f8cf9883c08c7bd5426a0a0c55f7be6a8169b69
Hayley Goodman <31858311+hayleyg5@users.noreply.github.com>
2020-06-02 22:04:42 -0400

    Update groceries.js

--- groceries.js
+++ groceries.js
@@ -124,10 +124,10 @@ function restrictListProducts(prods, restriction) {
 
 // Calculate the total price of items, with received parameter being a list of products
 function getTotalPrice(chosenProducts) {
-	let totalPrice = 0;
-	for (let i=0; i<chosenProducts.length; i+=1) {
-		if (chosenProducts.indexOf(chosenProducts[i].name) > -1){
-			totalPrice += chosenProducts[i].price;
+	totalPrice = 0;
+	for (let i=0; i<products.length; i+=1) {
+		if (chosenProducts.indexOf(products[i].name) > -1){
+			totalPrice += products[i].price;
 		}
 	}
 	return totalPrice;
